6dfbc5e1d9925148be63295dbe37add397693414
Rafael Chagas <chagas@gmail.com>
2019-02-24 20:01:34 -0300

    Added sample project and fixed minor issues
    Signed-off-by: Rafael Chagas <chagas@gmail.com>

---
+++ sample/src/main/java/com/rtchagas/pingsample/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.rtchagas.pingsample
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.libraries.places.api.model.Place
+import com.rtchagas.pingplacepicker.PingPlacePicker
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    private val pingActivityRequestCode = 1001
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnOpenPlacePicker.setOnClickListener {
+            showPlacePicker()
+        }
+    }
+
+    private fun showPlacePicker() {
+
+        val pingBuilder = PingPlacePicker.IntentBuilder()
+
+        pingBuilder.setAndroidApiKey(getString(R.string.key_google_apis_android))
+        pingBuilder.setGeolocationApiKey(getString(R.string.key_google_apis_geolocation))
+
+        startActivityForResult(pingBuilder.build(this), pingActivityRequestCode)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if ((requestCode == pingActivityRequestCode) && (resultCode == Activity.RESULT_OK)) {
+
+            val place: Place? = PingPlacePicker.getPlace(data!!)
+
+            toast("You selected: ${place?.name}")
+        }
+    }
+}

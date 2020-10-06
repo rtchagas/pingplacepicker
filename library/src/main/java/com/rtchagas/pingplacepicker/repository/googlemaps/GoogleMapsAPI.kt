6e0ba51134916c38904dd41509b573825d17de95
Jong-Ha Ahn <jongha.ahn@mrlatte.net>
2020-10-06 23:30:58 +0900

    Add language param for google api

--- library/src/main/java/com/rtchagas/pingplacepicker/repository/googlemaps/GoogleMapsAPI.kt
+++ library/src/main/java/com/rtchagas/pingplacepicker/repository/googlemaps/GoogleMapsAPI.kt
@@ -4,16 +4,18 @@ import com.rtchagas.pingplacepicker.model.SearchResult
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.*
 
 interface GoogleMapsAPI {
-
     @GET("place/nearbysearch/json?rankby=distance")
     fun searchNearby(@Query("location") location: String,
-                     @Query("key") apiKey: String)
+                     @Query("key") apiKey: String,
+                     @Query("language") language: String = Locale.getDefault().language)
             : Single<SearchResult>
 
     @GET("geocode/json")
     fun findByLocation(@Query("latlng") location: String,
-                       @Query("key") apiKey: String)
+                       @Query("key") apiKey: String,
+                       @Query("language") language: String = Locale.getDefault().language)
             : Single<SearchResult>
 }
